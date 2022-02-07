401968fbe4aca831412f3622f379d56a17abe41c
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-07 12:33:17 -0800

    connect to Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL)
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
